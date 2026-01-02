2afadb2847e0fe3a2c3e4107fc0c6e81edfa6004
Tech Testing First <techtestingfirst@gmail.com>
2026-01-02 17:20:47 +0000

    feat: add hosting plan, product, server

---
+++ hostfrapt/hostfrapt/doctype/hostfrapt_server/hostfrapt_server.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Praveen Kumar and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Hostfrapt Server", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ hostfrapt/hostfrapt/doctype/hostfrapt_server/hostfrapt_server.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Praveen Kumar and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class HostfraptServer(Document):
+	pass
